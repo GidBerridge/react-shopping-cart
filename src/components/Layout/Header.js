c8ebbfdd201fbc8c8966c1b1b790d69baff28777
Gid <gid@Gids-iMac.local>
2023-02-19 15:43:38 +0000

    all

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,21 +1,25 @@
 import React from 'react'
+// import { Container, Row } from 'react-bootstrap'
 
-import mealsImage from '../../assets/meals.jpeg'
+import guitarsImage from '../../assets/electric_1.jpg'
+import icon from '../../assets/wave.png'
 import classes from './Header.module.css'
 import HeaderCartButton from './HeaderCartButton'
 
 const Header = (props) => {
   return (
     <>
-      <header className={classes.header}>
-        <h1>ReactMeals</h1>
-        <HeaderCartButton onClick={props.onShowCart} />
-      </header>
-      <div className={classes['main-image']}>
-        <img src={mealsImage} alt="A table full of food" />
-      </div>
+      <header className={classes.header} >
+        <div>
+          <img src={icon} alt="wave icon" className={classes.headerIcon} />
+        </div> 
+        <h1 className="title"> Kahuna Guitars </h1> 
+        <HeaderCartButton onClick={props.onShowCart}/> 
+      </header> 
+        <div className={classes['main-image']} >
+          <img src={guitarsImage} alt="A selection of fine guitars" />
+        </div> 
     </>
-  )
-}
+     )}
 
-export default Header
+    export default Header
