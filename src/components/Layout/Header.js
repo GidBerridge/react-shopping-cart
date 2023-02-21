12fd18a94a5c5599ff595bd5986136c545d80cde
Gid <gid@Gids-iMac.local>
2023-02-21 13:15:46 +0000

    header_response

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import { Container, Row } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 
 import guitarsImage from '../../assets/electric_1.jpg'
 import icon from '../../assets/wave.png'
@@ -9,13 +9,21 @@ import HeaderCartButton from './HeaderCartButton'
 const Header = (props) => {
   return (
     <>
-      <header className={classes.header} >
-        <div>
-          <img src={icon} alt="wave icon" className={classes.headerIcon} />
-        </div> 
-        <h1 className={classes.headerTitle}> Kahuna Guitars </h1> 
-        <HeaderCartButton onClick={props.onShowCart}/> 
-      </header> 
+    <Container>
+          <header className={classes.header}>
+        {/* <div> */}
+          {/* <Col className={classes.headerKahuna}> */}
+            <div className={classes.headerKahuna}>
+            <img src={icon} alt="wave icon" className={classes.headerIcon} />
+          </div> 
+          <h1 className={classes.headerTitle}> Kahuna Guitars </h1> 
+        {/* </Col> */}
+        {/* <Col>    */}
+          <HeaderCartButton onClick={props.onShowCart}/> 
+        {/* </Col> */}
+            {/* </div> */}
+        </header> 
+    </Container>
         <div className={classes['main-image']} >
           <img src={guitarsImage} alt="A selection of fine guitars" />
         </div> 
